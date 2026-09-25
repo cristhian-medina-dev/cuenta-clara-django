a718ee2deda7f7b841dd49630c6d43a561206228
Cristhian <medinacristhian2020@gmail.com>
2026-09-25 02:11:26 -0300

    template estaba dando problemas lo movimos a cuentaclara

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+from gettext import install
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cuentaclara',
 ]
 
 MIDDLEWARE = [
